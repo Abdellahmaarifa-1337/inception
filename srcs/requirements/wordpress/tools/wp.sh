83f3e7fa49ac519dbc3591d49d1fdf45a63a091d
Abdellah maarifa <thefitnessfab@gmail.com>
2023-01-03 13:36:25 -0500

    update

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -9,13 +9,15 @@
 #su -u www-data
 mkdir -p /var/www/html &&\
 wp core download --path=/var/www/html --allow-root  &&\
-chown -R root:root  /var/www/html &&\
-chmod -R 777 /var/www/html &&\
+chown -R www-data:www-data  /var/www/html
+
 
 #touch /var/www/html/wp-config.php
 wp config create --path=/var/www/html --dbname=${WORDPRESS_DB_NAME} --dbuser=${WORDPRESS_DB_USER} --dbpass=${WORDPRESS_DB_PASSWORD} --dbhost=${WORDPRESS_DB_HOST} --allow-root --force
 if ! wp core is-installed --path=/var/www/html --allow-root; then
 	wp core install --url=${WORDPRESS_SITE_URL} --path=/var/www/html --title=${WORDPRESS_SITE_TITLE} --admin_user=${WORDPRESS_ADMIN_USER} --admin_password=${WORDPRESS_ADMIN_PASSWORD} --admin_email=${WORDPRESS_ADMIN_EMAIL} --allow-root
 	wp user create ${WORDPRESS_USER_NAME} ${WORDPRESS_USER_EMAIL} --role=${WORDPRESS_USER_ROLE} --user_pass=${WORDPRESS_USER_PASSWORD} --path=/var/www/html --allow-root
+	wp plugin install "Redis Object Cache" --activate --url=${WORDPRESS_SITE_URL} --path=/var/www/html --allow-root
+	wp plugin activate "redis-object-cache" --url=${WORDPRESS_SITE_URL} --path=/var/www/html --allow-root
 fi
 php-fpm8.1 -F
